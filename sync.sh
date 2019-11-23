c2b49eb63280f430770c5a1a0ee7a033dbaa18e7
Gojko Adzic <gojko@gojko.com>
2019-11-23 13:29:36 +0100

    use SameSite to remove chrome warning errors

---
+++ sync.sh
@@ -0,0 +1,2 @@
+aws s3 cp start.html s3://mindmup-3rdptycheck --cache-control="max-age=8640000" --acl public-read
+aws s3 cp complete.html s3://mindmup-3rdptycheck --cache-control="max-age=8640000" --acl public-read
